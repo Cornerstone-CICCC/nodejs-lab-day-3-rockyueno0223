2be6eb6c4c1e564ef0d837d197bd5bae22a3fa6e
Hiroki Ueno <rockyueno@gmail.com>
2024-11-29 14:03:56 -0800

    added send and receive message functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,54 @@
+import { useState, useEffect } from 'react';
+import { socket } from './sockets/socket';
+
+interface ChatData {
+  username: string;
+  message: string;
+}
+
 function App() {
+  const [message, setMessage] = useState("");
+  const [receivedMessages, setReceivedMessages] = useState<Array<ChatData>>([]);
+
+  useEffect(() => {
+    const handleMessage = (data: ChatData) => {
+      setReceivedMessages((prevMessages) => [...prevMessages, data]);
+    };
+
+    // Attach the listener
+    socket.on('newMessage', handleMessage);
+
+    return () => {
+      socket.off('newMessage', handleMessage);
+    };
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit('sendMessage', { message: message, username: socket.id })
+      setMessage('');
+    }
+  }
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message-input"
+          id=""
+          placeholder='Message'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send Message</button>
+      </form>
+      <ul>
+        {receivedMessages.map((msg) => (
+          <li>{msg.message}</li>
+        ))}
+      </ul>
     </>
   )
 }
